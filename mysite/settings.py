6be607818db65e816323fc9d9025517e18831f66
Jacob Bogle <jakebogle4@gmail.com>
2025-10-26 21:08:59 -0400

    Add security and session middleware to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # ... (middleware) ...
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
