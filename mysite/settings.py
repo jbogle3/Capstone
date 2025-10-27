301b2dd256b032ce3ebb1148ea338e2e1eb7657e
Jacob Bogle <jakebogle4@gmail.com>
2025-10-26 20:58:19 -0400

    Set DEBUG to True in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # ... (SECRET_KEY, DEBUG, ALLOWED_HOSTS) ...
-
+DEBUG = True
 
 # Application definition
 
