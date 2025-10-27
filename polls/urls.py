fe885b806ee4a6963182dd14151d943820df6ddc
Jacob Bogle <jakebogle4@gmail.com>
2025-10-26 20:37:18 -0400

    Add new URL paths for form and vulnerable pages

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("form/", views.form_page, name="form_page"),            # <--- ADD THIS LINE
+    path("vulnerable/", views.vulnerable_page, name="vulnerable_page"),  # <--- ADD THIS LINE
 ]
