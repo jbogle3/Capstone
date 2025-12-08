4adc3d4161cded082f77fbe5a9c275de76d7c777
Jacob Bogle <jakebogle4@gmail.com>
2025-12-08 13:43:16 -0500

    Add UploadedFile model for file uploads

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_name = models.CharField(max_length=255)
+    # Security Scan Results
+    has_dep = models.BooleanField(default=False)
+    has_aslr = models.BooleanField(default=False)
+    is_safe = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.file_name
