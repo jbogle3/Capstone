c40b0bedf33574b83937e2301c1a7ab8a805754a
Jacob Bogle <jakebogle4@gmail.com>
2025-12-10 09:34:24 -0500

    Add upload_analysis path to urlpatterns

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("form/", views.form_page, name="form_page"),            # <--- ADD THIS LINE
-    path("vulnerable/", views.vulnerable_page, name="vulnerable_page"),  # <--- ADD THIS LINE
+    path("form/", views.form_page, name="form_page"),
+    path("vulnerable/", views.vulnerable_page, name="vulnerable_page"),
+    path("upload/", views.upload_analysis, name="upload_analysis"),
 ]
